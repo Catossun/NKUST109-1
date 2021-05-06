9536675686968bb20c23d7a3c900970ec249b68f
Weikeup <boshun2009@gmail.com>
2021-05-06 11:42:15 +0800

    Create a REST API for accessing VolumeDatas

---
+++ NKUST109-2/Controllers/RESTApi/VolumeDatasApiController.cs
@@ -0,0 +1,40 @@
+﻿using KHLightTrail;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NKUST109_2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NKUST109_2.Controllers.RESTApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VolumeDatasApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public VolumeDatasApiController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("{year?}/{month?}")]
+        public Task<List<VolumeData>> GetVolumeDatas(int? year, int? month)
+        {
+            IQueryable<VolumeData> query = dbContext.VolumeDatas.AsQueryable();
+            if (year != null)
+            {
+                query = query.Where(vd => vd.Year == year);
+            }
+            if (month != null)
+            {
+                query = query.Where(vd => vd.Month == month);
+            }
+            return query.ToListAsync();
+        }
+    }
+}
