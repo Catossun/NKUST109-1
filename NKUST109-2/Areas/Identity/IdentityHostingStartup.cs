fb41a44fe6e5c65a6f09dd16ca7b5740c9955741
Weikeup <boshun2009@gmail.com>
2021-06-23 12:08:06 +0800

    Chage and remove some useless things in idenetity layout

---
+++ NKUST109-2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NKUST109_2.Data;
+
+[assembly: HostingStartup(typeof(NKUST109_2.Areas.Identity.IdentityHostingStartup))]
+namespace NKUST109_2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
